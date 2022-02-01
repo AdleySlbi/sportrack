069ab8414765e49af616234be4ef80b41fe007cc
AdSlb <adley.salabi@gmail.com>
2022-02-01 16:18:06 +0100

    Initial Commit

---
+++ Sportrack/SportrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportrackApp.swift
+//  Sportrack
+//
+//  Created by Adley Salabi on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SportrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
